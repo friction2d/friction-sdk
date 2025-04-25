1155385dd277334e86490580ee54d9126e530035
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2025-04-25 15:59:04 +0200

    Create build_linux_ci.sh

---
+++ build_linux_ci.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Friction - https://friction.graphics
+#
+# Copyright (c) Ole-André Rodlie and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+set -e -x
+
+CWD=`pwd`
+
+COMMIT=`git rev-parse --short=8 HEAD`
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+BUILD_ENGINE=OFF \
+LOCAL_BUILD=0 \
+REL=0 \
+BRANCH=${BRANCH} \
+COMMIT=${COMMIT} \
+CUSTOM=CI \
+./build_linux.sh
