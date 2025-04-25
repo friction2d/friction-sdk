5aa6ada3cb82a429b412a9f160666e17ee0f9a73
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2025-04-25 15:17:15 +0200

    Update

--- run_vfxplatform.sh
+++ run_vfxplatform.sh
@@ -33,6 +33,12 @@ DOWNLOAD_SDK=${DOWNLOAD_SDK:-0}
 SDK_VERSION="1.0.0"
 TAR_VERSION=${TAR_VERSION:-""}
 
+URL=https://github.com/friction2d/friction-sdk/releases/download/v${SDK_VERSION}
+APPIMG=20240401
+APPIMAGE_TAR=friction-appimage-tools-${APPIMG}.tar.xz
+SDK_TAR=friction-sdk-${SDK_VERSION}r5-linux-x86_64.tar.xz
+SKIA_TAR=skia-friction-${SDK_VERSION}-f5941b02-linux-x86_64.tar.xz
+
 DOCKER="docker run"
 DOCKER="${DOCKER} -e BUILD_ENGINE=${BUILD_ENGINE} -e REL=${REL} -e MKJOBS=${MKJOBS} -e TAR_VERSION=${TAR_VERSION} -e SDK_VERSION=${SDK_VERSION} -e ONLY_SDK=${ONLY_SDK} -e DOWNLOAD_SDK=${DOWNLOAD_SDK} -e BRANCH=${BRANCH} -e COMMIT=${COMMIT} -e TAG=${TAG} -e CUSTOM=${CUSTOM}"
 DOCKER="${DOCKER} -t --mount type=bind,source=${CWD}/distfiles,target=/mnt"
@@ -47,8 +53,28 @@ if [ ! -d "${CWD}/distfiles/sdk" ]; then
     mkdir -p ${CWD}/distfiles/sdk
 fi
 
+if [ "${DOWNLOAD_SDK}" = 1 ]; then
+    cd ${CWD}/distfiles
+    if [ ! -d "linux" ]; then
+        if [ ! -f "${APPIMAGE_TAR}" ]; then
+            wget ${URL}/${APPIMAGE_TAR}
+        fi
+        tar xvf ${APPIMAGE_TAR}
+    fi
+    if [ ! -d "skia" ]; then
+        if [ ! -f "${SKIA_TAR}" ]; then
+            wget ${URL}/${SKIA_TAR}
+        fi
+        tar xvf ${SKIA_TAR}
+    fi
+    cd ${CWD}/distfiles/sdk
+    if [ ! -f "${SDK_TAR}" ]; then
+        wget ${URL}/${SDK_TAR}
+    fi
+fi
+
 if [ "${LOCAL_BUILD}" = 1 ]; then
-    (cd vfxplatform; docker build -t friction-vfxplatform .)
+    (cd ${CWD}/vfxplatform; docker build -t friction-vfxplatform .)
     ${DOCKER} friction-vfxplatform
 else
     docker pull frictiongraphics/friction-vfxplatform-sdk
